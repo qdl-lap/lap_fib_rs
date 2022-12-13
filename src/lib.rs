8f5faf0f9c3b9140dfd48f8c0497948807689280
Phu La <la_phu@yahoo.com>
2022-12-13 08:54:06 +0000

    initial rust_py integration 3

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+mod fib_calcs;
+
+use fib_calcs::fib_number::__pyo3_get_function _fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function _fibonacci_numbers;
+pub mod fib_numbers;
 
 #[pyfunction]
 fn say_hello() {
@@ -9,10 +14,12 @@ fn say_hello() {
 
 #[pymodule]
 fn lap_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    match m.add_wrapped(wrap_pyfunction!(say_hello)) {
-        Err(e) => println!("{:?}", e),
-        _ => ()
-    }
+    // match m.add_wrapped(wrap_pyfunction!(say_hello)) {
+    //     Err(e) => println!("{:?}", e),
+    //     _ => ()
+    // }
+    m.add_wrapped(wrap_pyfunction!(say_hello));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     Ok(())
 }
-
